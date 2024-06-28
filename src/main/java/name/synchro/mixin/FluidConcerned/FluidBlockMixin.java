9defd4d4f5ebd7b7e9229570db20baf8a01efe9a
Pajang Forever <2548531078@qq.com>
2024-06-28 16:28:59 +0800

    test update 20240628

--- src/main/java/name/synchro/mixin/FluidConcerned/FluidBlockMixin.java
+++ src/main/java/name/synchro/mixin/FluidConcerned/FluidBlockMixin.java
@@ -2,13 +2,11 @@ package name.synchro.mixin.FluidConcerned;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import name.synchro.Synchro;
 import name.synchro.fluids.FluidUtil;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.FluidBlock;
 import net.minecraft.block.ShapeContext;
-import net.minecraft.fluid.FluidState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
@@ -21,20 +19,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(FluidBlock.class)
 public class FluidBlockMixin {
-    /**
-     * {@link FluidBlock#getFluidState} should not be called due to the new format of fluid storage in chunk.
-     * Fluid states are no longer stored in block states. If it's invoked by another mod, please report this to the developers.
-     * It's recommended to use {@link World#getFluidState} with BlockPos parameter instead or the game may not run as expected.
-     */
-    @Inject(method = "getFluidState", at = @At("HEAD"))
-    private void warnsWhenGetFluidState(BlockState state, CallbackInfoReturnable<FluidState> cir) {
-        Synchro.LOGGER.warn("FluidBlock#getFluidState has been called unexpectedly.");
-        if (!Synchro.fluidStorageWarned){
-            Synchro.LOGGER.debug("FluidBlock#getFluidState should not be called due to the new format of fluid storage in chunk. Fluid states are no longer stored in block states. If it's invoked by another mod, please report this to the developers. It's recommended to use World#getFluidState with BlockPos parameter instead or the game may not run as expected.");
-            Synchro.fluidStorageWarned = true;
-        }
-    }
-
     @Inject(method = "getOutlineShape", at = @At("RETURN"), cancellable = true)
     private void debugGetOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context, CallbackInfoReturnable<VoxelShape> cir){
         cir.setReturnValue(VoxelShapes.fullCube());
